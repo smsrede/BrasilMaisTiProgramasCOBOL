8fbf1c0ff9190a7c430f81a13c8bd5574eddee08
Samuel Menezes <smsrede@gmail.com>
2017-04-18 18:03:16 -0300

    Aulas de COBOL do BrasilMaisTI
    Alguns dos algorótmos de exemplos usados nas aulas de COBOL do
    BrasilMaisTI

---
+++ Class1.cbl
@@ -0,0 +1,13 @@
+       class-id ClassLibrary1.Class1.
+       
+       working-storage section.
+
+       method-id InstanceMethod.
+       local-storage section.
+       procedure division.
+           display "hello babaca".
+
+           goback.
+       end method.
+
+       end class.
